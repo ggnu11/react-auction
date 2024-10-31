8206d6cf00549d23dd6b99628eda3e16119f6636
Choi-Yeonghun <choiyounghun0712@gmail.com>
2024-10-31 15:36:09 +0900

    [Layout] 메뉴 생성 시, 기본 레이아웃 구조가 잡혀있는지 테스트 작업

--- src/components/layout/NewLayout.tsx
+++ src/components/layout/NewLayout.tsx
@@ -3,13 +3,10 @@ import { Footer } from 'antd/es/layout/layout';
 import { Outlet } from 'react-router';
 import NewHeader from './header/NewHeader';
 import SideMenu from './sider/SideMenu';
-import { useTranslation } from 'react-i18next';
 
 const { Content } = Layout;
 
 const NewLayout = () => {
-  const { t } = useTranslation('translation');
-
   return (
     <Layout hasSider>
       <SideMenu />
@@ -17,7 +14,6 @@ const NewLayout = () => {
         <NewHeader />
         <Content className="px-12 pt-12 flex-1 overflow-y-auto max-h-[calc(100vh-65px)]">
           <div className="bg-white h-full">
-            {t('title')}
             <Outlet />
           </div>
           <Footer className="text-center text-base h-[40px] flex items-center justify-center">
