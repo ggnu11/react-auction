d0c210bbc102909f7dc08db3226f5be995f7139d
Choi-Yeonghun <choiyounghun0712@gmail.com>
2024-12-19 15:16:14 +0900

    [Dashboard] 기본 파일 구조 생성

--- src/components/pages/dashboard/Dashboard.tsx
+++ src/components/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,17 @@
+import PopularProductList from '../../product/popular-product';
+import ProductList from '../../product';
+
 const Dashboard = () => {
-  return <div>test</div>;
+  return (
+    <div className="flex flex-col">
+      <div>
+        <PopularProductList />
+      </div>
+      <div>
+        <ProductList />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
