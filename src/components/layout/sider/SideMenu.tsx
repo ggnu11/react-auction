05481b80e73bd21ec58bab2417fdc3e700426349
Choi-Yeonghun <choiyounghun0712@gmail.com>
2024-12-24 10:57:47 +0900

    [Sidebar] 메뉴 컴포넌트 추가

--- src/components/layout/sider/SideMenu.tsx
+++ src/components/layout/sider/SideMenu.tsx
@@ -1,9 +1,9 @@
-import { HomeOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { BankOutlined, HomeOutlined, TeamOutlined } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
 import { useRecoilValue } from 'recoil';
 import { isOpenSideMenu } from '../../../recoil/layout';
-import { useNavigate } from 'react-router';
-import { useTranslation } from 'react-i18next';
 
 const { Sider } = Layout;
 
@@ -16,6 +16,28 @@ const SideMenu = () => {
   const handleMenuClick = (path: string) => {
     navigate(path);
   };
+
+  const menuItems = [
+    {
+      key: '1',
+      icon: <HomeOutlined />,
+      label: t('home'),
+      onClick: () => handleMenuClick('/main'),
+    },
+    {
+      key: '2',
+      icon: <BankOutlined />,
+      label: t('auction'),
+      onClick: () => handleMenuClick('/auction'),
+    },
+    {
+      key: '3',
+      icon: <TeamOutlined />,
+      label: t('support'),
+      onClick: () => handleMenuClick('/support'),
+    },
+  ];
+
   return (
     <Sider
       className={`bg-white shadow-lg overflow-auto h-screen fixed inset-y-0 z-[5] left-0 scrollbar-thin scrollbar-gutter-stable transition-transform duration-300 ${
@@ -25,17 +47,7 @@ const SideMenu = () => {
       <div className="p-4 border-b border-gray-200">
         <div className="text-xl font-bold text-center">Logo</div>
       </div>
-      <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} className="mt-14">
-        <Menu.Item key="1" icon={<HomeOutlined />} onClick={() => handleMenuClick('/main')}>
-          {t('home')}
-        </Menu.Item>
-        <Menu.Item key="2" icon={<UserOutlined />} onClick={() => handleMenuClick('/profile')}>
-          {t('auction')}
-        </Menu.Item>
-        <Menu.Item key="3" icon={<SettingOutlined />} onClick={() => handleMenuClick('/settings')}>
-          {t('support')}
-        </Menu.Item>
-      </Menu>
+      <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} className="mt-14" items={menuItems} />
     </Sider>
   );
 };
