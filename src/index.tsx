99125fb595f2737186f5123db2943051008ed393
Choi-Yeonghun <choiyounghun0712@gmail.com>
2025-01-02 09:45:15 +0900

    [폴더] Locales 폴더명 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import App from './App';
-import './locales/i18n';
+import '@config/i18n';
 import './assets/css/tailwind.css';
 
 const queryClient = new QueryClient({
