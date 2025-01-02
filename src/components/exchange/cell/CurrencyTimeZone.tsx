bb744f5e800af5d5307c3f7ed82ed72e574ca64e
Choi-Yeonghun <choiyounghun0712@gmail.com>
2025-01-02 11:42:41 +0900

    [Dashboard] 통화별 현재 시간 기능 보완

--- src/components/exchange/cell/CurrencyTimeZone.tsx
+++ src/components/exchange/cell/CurrencyTimeZone.tsx
@@ -2,22 +2,20 @@ import { baseCurrencyState, targetCurrencyState } from '@store/dashboard';
 import { useRecoilValue } from 'recoil';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import { timezones } from '../timezone/timezones';
 
 const CurrencyTimeZone = () => {
   const baseCurrency = useRecoilValue(baseCurrencyState);
   const targetCurrency = useRecoilValue(targetCurrencyState);
 
-  const timezones: { [key: string]: string } = {
-    USD: 'America/New_York',
-    KRW: 'Asia/Seoul',
-  };
-
   return (
     <div className="mt-4">
       <p>
+        {baseCurrency} :
         <Moment format="YYYY-MM-DD HH:mm:ss" tz={timezones[baseCurrency]} interval={1000} />
       </p>
       <p>
+        {targetCurrency} :
         <Moment format="YYYY-MM-DD HH:mm:ss" tz={timezones[targetCurrency]} interval={1000} />
       </p>
     </div>
