e4562d9515d4158f9447c8239ed04137ac589c33
Choi-Yeonghun <choiyounghun0712@gmail.com>
2025-01-06 16:19:45 +0900

    [Exchange] FilterDateCell 보완

--- src/components/exchange/cell/FilterDateCell.tsx
+++ src/components/exchange/cell/FilterDateCell.tsx
@@ -17,15 +17,19 @@ const FilterDateCell = ({ title, fieldName }: FilterDateCellProps) => {
         name={fieldName}
         rules={[
           { required: true, message: t(`validation_${fieldName}`) },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              const from = getFieldValue('from');
-              if (!value || !from || dayjs(value).isAfter(dayjs(from))) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error(t('validation_to_before_from')));
-            },
-          }),
+          ...(fieldName === 'to'
+            ? [
+                ({ getFieldValue }: any) => ({
+                  validator(_: any, value: any) {
+                    const from = getFieldValue('from');
+                    if (!value || !from || dayjs(value).isAfter(dayjs(from))) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error(t('validation_to_before_from')));
+                  },
+                }),
+              ]
+            : []),
         ]}
       >
         <DatePicker className="w-full" />
@@ -33,4 +37,5 @@ const FilterDateCell = ({ title, fieldName }: FilterDateCellProps) => {
     </div>
   );
 };
+
 export default FilterDateCell;
